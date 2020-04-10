de80e654510bb4de02f7476bbece798755a70df4
Rénes Péter <renes.peter@microsec.hu>
2020-04-10 19:10:15 +0200

    RxJava3

--- app/src/main/java/hu/renes/kotlin/domain/service/JobService.kt
+++ app/src/main/java/hu/renes/kotlin/domain/service/JobService.kt
@@ -8,7 +8,7 @@ import retrofit2.http.GET
 
 interface JobService {
 
-    @GET("Sonkagyerek/9058c68baf1893e0fea4a3eb1d586f3b/raw/409717fd0c3ca1e2cc4621fe9c22425a3f0bce00/test.json")
+    @GET("Sonkagyerek/9058c68baf1893e0fea4a3eb1d586f3b/raw/df73b753d8b22a56996510a2d849b3c83958782d/test.json")
     fun getExperience(): Single<Candidate>
 
     object Factory {
--- app/src/main/java/hu/renes/kotlin/injection/module/NetworkModule.kt
+++ app/src/main/java/hu/renes/kotlin/injection/module/NetworkModule.kt
@@ -7,6 +7,7 @@ import com.squareup.picasso.OkHttp3Downloader
 import com.squareup.picasso.Picasso
 import dagger.Module
 import dagger.Provides
+import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory
 import hu.renes.kotlin.BuildConfig.BASE_URL
 import hu.renes.kotlin.domain.service.JobService
 import hu.renes.kotlin.injection.qualifier.ApplicationContext
@@ -15,7 +16,6 @@ import okhttp3.logging.HttpLoggingInterceptor
 import okhttp3.logging.HttpLoggingInterceptor.Level.BODY
 import org.koin.android.BuildConfig
 import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
@@ -34,7 +34,7 @@ class NetworkModule {
             .baseUrl(BASE_URL)
             .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create(gson))
-            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
             .build()
     }
 
